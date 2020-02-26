edbd4725596767a8c6edcc6b745bbf0d5b500d41
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-02-26 03:51:42 +0100

    Implement weighted graph

---
+++ WeightedGraph/Java/src/com/ssanusi/Main.java
@@ -0,0 +1,19 @@
+package com.ssanusi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	var graph = new WeightedGraph();
+	graph.addNode("A");
+	graph.addNode("B");
+	graph.addNode("C");
+	graph.addNode("D");
+	graph.addEdge("A", "B", 3);
+	graph.addEdge("B", "D", 4);
+	graph.addEdge("C", "D", 5);
+	graph.addEdge("A", "C", 1);
+	graph.addEdge("B", "C", 2);
+	var tree = graph.getMinimumSpanningTree();
+	tree.print();
+    }
+}
